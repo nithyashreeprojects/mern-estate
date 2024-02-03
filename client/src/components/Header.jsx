26bdb34cd30940b7ac49191b12f0c5de2649ebfb
Nithyashree R <getnrk123@gmail.com>
2024-02-03 23:31:24 +0530

    Add redux persist
    Co-authored-by: Srividya <Srividya083@users.noreply.github.com>
    Co-authored-by: mayuri23palayil <mayuri23palayil@users.noreply.github.com>

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -10,7 +12,7 @@ export default function Header() {
                     <span className ='text-slate-500'>Sameera</span>
                     <span className ='text-slate-700'>Estates</span>
                 </h1>
-            </Link>
+         ``   </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input type="text" placeholder="Search" 
                 className='bg-transparent focus:outline-none w-24 sm:w-64'/>
@@ -23,9 +25,15 @@ export default function Header() {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to='/sign-in'>
-                    <li className='text-slate-700 hover:underline'>Sign In</li>
-                </Link>
+                <Link to='/profile'>
+                {currentUser ? (
+                   <img className ='rounded-full h-7 w-7 object-cover' src= {currentUser.avatar} alt='profile' />
+                ) : <li className='text-slate-700 hover:underline'>Sign In</li>
+                
+                }
+                 </Link>
+                    
+                
             </ul>
         </div>
     </header>
