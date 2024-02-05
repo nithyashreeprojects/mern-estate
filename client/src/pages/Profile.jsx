bd8809df5d71a17e55811baf2af588e6ca21c803
Nithyashree R <getnrk123@gmail.com>
2024-02-05 22:34:42 +0530

    Image upload functionality
    Co-authored-by: mayuri23palayil <mayuri23palayil@users.noreply.github.com>

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,75 @@
 import { useSelector } from 'react-redux'
+import { useRef,useState,useEffect } from 'react'
+import {getStorage,ref} from 'firebase/storage';
+import {app} from '../firebase';
+
 
 
 export default function Profile() {
-  const {currentUser} = useSelector((state)=> state.user)
+  const fileRef=useRef(null);
+  const {currentUser} = useSelector((state)=> state.user);
+  const [file,setFile]=useState(undefined);
+  const [filePerc,setFilePerc]=useState(0);
+  const [fileUploadError,setFileUploadError]=useState(false);
+  
+ const [formData,setFormData]=useState({});
+
+  useEffect(()=>{
+if(file){
+  handleFileUpload(file);
+}
+  },[file]);
+
+  const handleFileUpload=(file)=>{
+    const storage=getStorage(app);
+    const fileName=new Date().getTime()+file.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask=uploadBytesResumable(storageRef,file);
+
+    uploadTask.on('state_changed',
+    (snapshot)=>{
+      const progress=(snapshot.bytesTransferred /
+      snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progress));
+
+    });
+    (error)=>
+    {
+      setFileUploadError(true);
+    };
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL)=>{
+          setFormData
+        }
+    }
+
+    
+  };
+  
+
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id] : e.target.value});
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center
       my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" 
+        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt="profile" 
         className='rounded-full h-24 w-24 object-cover
         cursor-pointer self-center mt-2'/>
         <input type="text" placeholder='username'
-        id='username' className='border p-3 rounded-lg' />
+        defaultValue={currentUser.username}
+        id='username' className='border p-3 rounded-lg' onChange={handleChange}/>
         <input type="email" placeholder='email'
-        id='email' className='border p-3 rounded-lg' />
+        id='email' defaultValue={currentUser.email} 
+        className='border p-3 rounded-lg' onChange={handleChange}/>
         <input type="text" placeholder='password'
-        id='password' className='border p-3 rounded-lg' />
+        id='password' className='border p-3 rounded-lg' onChange={handleChange}/>
         <button className='bg-slate-700 text-white 
         rounded-lg p-3 uppercase hover:opacity-95
         diabled:opacity-80'>update</button>
