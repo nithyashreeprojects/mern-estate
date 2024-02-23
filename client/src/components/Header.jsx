11f149bf509f3c97217dbe6842fcc1e4a5ded2c8
Nithyashree R <getnrk123@gmail.com>
2024-02-23 22:06:55 +0530

    complete listings functionality
    Co-authored-by: mayuri23palayil <mayuri23palayil@users.noreply.github.com>

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/" className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Perumal</span>
+          <span className="text-slate-500">Home4U</span>
           <span className="text-slate-700">Estates</span>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
