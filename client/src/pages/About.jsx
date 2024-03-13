c847a738e17c2da7ef25cae47d8fa4f7e6f8ed58
Nithyashree R <getnrk123@gmail.com>
2024-03-13 23:15:29 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4
+      text-slate-800'>About Homes4U</h1>
+      <p className='mb-4 text-slate-700'>At Homes4U, we believe that 
+        finding the perfect home should be a 
+        seamless and rewarding experience. As a premier 
+        real estate company, we are dedicated to 
+        helping individuals, families, and investors 
+        achieve their property goals with confidence and ease.</p>
+      <p className='mb-4 text-slate-700'>With a passion for excellence and a commitment to integrity,
+        Homes4U sets the standard for exceptional 
+        service in the real estate industry. 
+        Our team of experienced professionals possesses 
+        unparalleled knowledge of local markets, 
+        ensuring that our clients receive expert guidance 
+        every step of the way.</p>
+      <p className='mb-4 text-slate-700'>At Homes4U, we embrace innovation and leverage 
+        cutting-edge technology to streamline the real 
+        estate process, making it more efficient and 
+        convenient for our clients. From advanced 
+        market analysis to immersive virtual tours, we 
+        harness the power of technology to deliver 
+        unparalleled results. Whether you're 
+        embarking on your real estate 
+        journey for the first time or 
+        you're a seasoned investor, Homes4U is your trusted partner every 
+        step of the way. Experience the difference with 
+        Homes4U and let us help you find the home of 
+        your dreams.</p>
+    </div>
   )
 }
